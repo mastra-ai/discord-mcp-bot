cb486956685afe1603570b0a57bb10b048340912
NikAiyer <nick.aiyer@gmail.com>
2025-03-24 18:31:47 -0500

    removed build

--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -2,8 +2,6 @@ import { openai } from "@ai-sdk/openai";
 import { Agent } from "@mastra/core/agent";
 import { MCPConfiguration } from "@mastra/mcp";
 
-console.log("PATH", process.env.PATH);
-
 const mcpConfig = new MCPConfiguration({
   servers: {
     mastra: {
